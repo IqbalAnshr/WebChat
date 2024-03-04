35705c97d721e4346aa5a7c0bc571c00e7616ec3
IqbalAnshr <iqbalanshr@gmail.com>
2024-03-04 11:44:11 +0700

    mobile responsive

--- php/config.php
+++ php/config.php
@@ -1,8 +1,16 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "webchat";
+
+require_once __DIR__ . '/../vendor/autoload.php';
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+
+$servername = $_ENV['DB_HOST'];
+$username = $_ENV['DB_USER'];
+$password = $_ENV['DB_PASS'];
+$dbname = $_ENV['DB_NAME'];
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
